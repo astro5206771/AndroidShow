771743535b942af52580d48c3c2e42c878a9287d
leijie <leijie@asiainfo.com>
2018-07-05 23:11:21 +0800

    初始化

---
+++ app/src/main/java/com/rj/myandroiddisplay/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rj.myandroiddisplay
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+1,activity 之间通信方式
+
+2，activity 与 service之间通信方式
